75b8f6554072e95712922f81a1ca54e6621864a5
Felipe Carrasco <felipe@bakers.dev>
2021-05-03 19:07:17 -0400

    Initial Commit

---
+++ RegistrationForm/RegistrationFormApp.swift
@@ -0,0 +1,17 @@
+//
+//  RegistrationFormApp.swift
+//  RegistrationForm
+//
+//  Created by Pipe Carrasco on 03-05-21.
+//
+
+import SwiftUI
+
+@main
+struct RegistrationFormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
